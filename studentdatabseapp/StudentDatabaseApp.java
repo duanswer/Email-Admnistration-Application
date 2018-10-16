1d625ad8d75307bde048e672467b1200aa847224
Eduardo Kediamosiko Nzinga <eduardo.nzinga@sistemafiergs.com.br>
2018-10-16 15:00:15 -0300

    Project completed

--- studentdatabseapp/StudentDatabaseApp.java
+++ studentdatabseapp/StudentDatabaseApp.java
@@ -17,9 +17,14 @@ public class StudentDatabaseApp {
 			students[i] = new student();		
 			students[i].enroll();
 			students[i].payTuition();
-			System.out.println(students[i].toString());
+			
 		}
 		
+		for (int i = 0; i<numberOfStudents; i++) {
+			System.out.println("");
+			System.out.println("Student " + (i+1) + " information: ");
+			System.out.println(students[i].toString());
+		}
 		in.close();
 	}
 

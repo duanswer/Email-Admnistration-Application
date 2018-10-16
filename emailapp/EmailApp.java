86a0ccc086048cf40aabc023e8b0dc07adb2501b
Eduardo Kediamosiko Nzinga <eduardo.nzinga@sistemafiergs.com.br>
2018-10-16 08:33:34 -0300

    Fixed for all users

--- emailapp/EmailApp.java
+++ emailapp/EmailApp.java
@@ -1,10 +1,19 @@
 package emailapp;
-
+import java.util.Scanner;
 public class EmailApp {
 
 	public static void main(String[] args) {
-		Email em = new Email("Eduardo", "Nzinga");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Type your first name: ");
+		String firstName = sc.nextLine();
+		System.out.println("Type your last name: ");
+		String lastName = sc.nextLine();
+		
+		Email em = new Email(firstName, lastName);
 		System.out.println(em.showInfo());
+		
+		sc.close();
 
 	}
 
